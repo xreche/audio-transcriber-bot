98816fa18195981b3fb78ccad08d40190cf7fa8e
Xavi <xavitecnologys@gmail.com>
2026-03-06 21:02:06 +0100

    chore: remove debug logging

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import os
 import logging
 from dotenv import load_dotenv
 
@@ -8,10 +7,6 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
-
-logger = logging.getLogger(__name__)
-logger.info(f"TELEGRAM_TOKEN present: {bool(os.getenv('TELEGRAM_TOKEN'))}")
-logger.info(f"GROQ_API_KEY present: {bool(os.getenv('GROQ_API_KEY'))}")
 
 from src.bot import run_bot
 
